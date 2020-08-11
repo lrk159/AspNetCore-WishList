4ff6d495192a5fe8984bd1d27df0f92e233e9ad1
Luc Kuckartz <luc.kuckartz@gmail.com>
2020-08-11 15:14:43 -0600

    adding more data and model objects

---
+++ WishList/Model/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Model
+{
+    public class Item
+    {
+
+        public int id;
+
+        [Required]
+        [MaxLength(50)]
+        public string Description;
+
+    }
+}
